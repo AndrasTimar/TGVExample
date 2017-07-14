e239233e1b88eb3cd4432e4f1a4f8f0cfef735c4
Andras <bandi@almosttrio.com>
2017-07-14 22:42:01 +0200

    fix View Dictionary issue

--- src/AndrasTimarTGV/Components/LoginFormViewComponent.cs
+++ src/AndrasTimarTGV/Components/LoginFormViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AndrasTimarTGV.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AndrasTimarTGV.Components
 {
@@ -6,7 +7,7 @@ namespace AndrasTimarTGV.Components
     {
         public IViewComponentResult Invoke()
         {
-            return View();
+            return View(new LoginUserViewModel());
         }
     }
 }
