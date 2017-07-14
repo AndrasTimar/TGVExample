9dcb6d2954dc879a0e9e955ce4082702d8ed7477
Andras <bandi@almosttrio.com>
2017-07-14 22:43:59 +0200

    remove unused admin functionality

--- src/AndrasTimarTGV/Models/ViewModels/UserViewModel.cs
+++ src/AndrasTimarTGV/Models/ViewModels/UserViewModel.cs
@@ -64,25 +64,5 @@ namespace AndrasTimarTGV.Models.ViewModels
         public string Password { get; set; }
     }
 
-    public class RoleEditModel
-    {
-        public IdentityRole Role { get; set; }
 
-        public IEnumerable<AppUser> Members { get; set; }
-
-        public IEnumerable<AppUser> NonMembers { get; set; }
-    }
-
-    public class RoleModificationModel
-    {
-        [Required]
-
-        public string RoleName { get; set; }
-
-        public string RoleId { get; set; }
-
-        public string[] IdsToAdd { get; set; }
-
-        public string[] IdsToDelete { get; set; }
-    }
 }
